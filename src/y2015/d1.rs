29af8a92b4f1cf77a1423de3b124297b1d5c582a
aliibrahim123 <ali.ibrahim.dev.1548@gmail.com>
2025-12-02 16:59:15 +0200

    y2025 d1 finished

--- src/y2015/d1.rs
+++ src/y2015/d1.rs
@@ -1,5 +1,19 @@
 use anyhow::Result;
 
 pub fn run(input: String) -> Result<()> {
+	let mut floor = 0;
+	for char in input.chars() {
+		floor += if char == '(' { 1 } else { -1 };
+	}
+	println!("part 1: {floor}");
+
+	let mut floor = 0;
+	for (ind, char) in input.chars().enumerate() {
+		floor += if char == '(' { 1 } else { -1 };
+		if floor == -1 {
+			println!("part 2: {}", ind + 1);
+			break;
+		}
+	}
 	Ok(())
 }
